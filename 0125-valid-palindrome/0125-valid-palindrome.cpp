a0d6fcd577c2269220eddd5a3a1a7acfe321ef73
Akansha Kukshal <akanshakukshal@gmail.com>
2026-02-10 23:23:51 +0530

    Time: 0 ms (100%), Space: 10.2 MB (24.39%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool check(int i,string &s)
+    {
+        if(i>=s.size()/2){
+            return true;
+        }
+        if(s[i]!=s[s.size()-i-1]){
+            return false;
+        }
+        return check(i+1,s);
+
+    }
+    bool isPalindrome(string s) {
+        string neww="";
+        for(char c:s){
+            if(isalnum(c))
+            {
+                neww+=tolower(c);
+
+            }
+
+        }
+        
+        return check(0,neww);
+
+        
+    }
+};
