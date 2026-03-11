a90c88566f878ac97dfa8910854f3fd4a3eba211
Akansha Kukshal <akanshakukshal@gmail.com>
2026-03-11 19:20:37 +0530

    Time: 0 ms (100%), Space: 8 MB (97.45%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        int freq[26]={0};
+        for(char c:s)
+        {
+            freq[c-'a']++;
+            if(freq[c-'a']==2) return c;
+        }
+        return ' ';
+        
+    }
+};
